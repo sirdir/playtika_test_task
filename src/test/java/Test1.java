de249de479faa7d54097f691b2d236f3ee4c875f
sirdir <phddir@gmail.com>
2017-01-23 08:59:36 +0200

    init commit with test classes, xml, groovy and hookers

---
+++ src/test/java/Test1.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+/**
+ * Created by sirdir on 23.01.17.
+ */
+public class Test1 {
+
+    @Test
+    public void testMethod1(){
+
+    }
+
+    @Test
+    public void testMethod2(){
+
+    }
+
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created by sirdir on 23.01.17.
+ */
+public class Test2 {
+
+    @Test
+    public void testMethod1(){
+
+    }
+
+    @Test
+    public void testMethod2(){
+
+    }
+}
